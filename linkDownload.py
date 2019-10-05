b5049a18e059b023e6c1067de24eb917f49ba31d
Adrià Carrasquilla <carrasquilla.adria@gmail.com>
2019-10-05 23:51:08 +0200

    Alguna modificación para que dani vea

--- linkDownload.py
+++ linkDownload.py
@@ -8,6 +8,7 @@ import json
 import sys
 from lxml import html
 import requests
+from collections import Counter
 
 #Abrir pagina web i adquirir texto
 url = "https://www.freecodecamp.org/news/my-first-python-project-converting-a-disorganized-text-file-into-a-neatly-structured-csv-file-21f4c6af502d"
@@ -15,7 +16,7 @@ file = 'test'
 
 
 
-def urlToList(url, fileName):
+def urlToList(url, fileName, dominio):
 	req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 	webpage = urlopen(req).read()
 	soup = BeautifulSoup(webpage, features="lxml")
@@ -34,24 +35,28 @@ def urlToList(url, fileName):
 	
 	#trabajaremos siempre con las palabras en minúsculas para evitar possibles errores de búsqueda
 	text = text.lower()
+	counts = Counter(text.split())
 	x={
-		"lista" : text.split()
+		"Dominio" : dominio,
+		"Link": url,
+		"Palabras": counts
 	}
 
 	#Passamos el texto como lista de palabras en un fichero
-	with open(fileName+'.json', 'w') as filehandle:
-	    json.dump(x, filehandle, ensure_ascii = False)
+	#with open(fileName+'.json', 'w') as filehandle:
+	#    json.dump(x, filehandle, ensure_ascii = False)
 
 	return True
 
 
-def readLinks(fileName):
+def readLinks():
 
 	rutaGoogle = 'http://www.google.com/search?btnG=1&q=site%3A'
 	parametros = sys.argv
 
 	if len(parametros) == 3:
 		url = parametros[1]
+		urlSrc= "".join(parametros[1])
 		url = (url.split("//"))[1]
 		tema = parametros[2]
 		pagina = requests.get(rutaGoogle + url)
@@ -61,13 +66,13 @@ def readLinks(fileName):
 		for elementos in tree.xpath("//a"):
 			ruta = elementos.get("href")
 			if "/url?q" in ruta:
-				arrayUrls.append("http://www.google.com"+elementos.get("href"))
+				arrayUrls.append(elementos.get("href").replace("/url?q=",""))#"http://www.google.com"+elementos.get("href"))
 		
 		i=0
 
 		for link in arrayUrls[:-1]:
 			print("reading link: "+ link)
-			urlToList(link, "test"+str(i))
+			urlToList(link, "test"+str(i), urlSrc)
 			i+=1
 	else:
 		print('Error input')
@@ -77,8 +82,7 @@ def readLinks(fileName):
 #url = "https://www.elmundo.es/pais-vasco/2019/07/10/5d25f9af21efa0c0578b456f.html"
 #urlToList(url,file)
 
-fitxer= "links.json"
-readLinks(fitxer)
+readLinks()
 
 
 
