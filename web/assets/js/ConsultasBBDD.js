7e85f2dee9fdf25015fb8ee158e081673346d55d
Daniel <danielortegaberja@gmail.com>
2019-10-06 03:24:00 +0200

    consultasbbdd

--- web/assets/js/ConsultasBBDD.js
+++ web/assets/js/ConsultasBBDD.js
@@ -1,5 +1,17 @@
-function getAjax(url,success){
-    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-    xhr.open('GET', url);
-
+function getAjax() {
+    $.ajax({
+        url: "../ObtenerListadoDominios.py",
+        type: "GET",
+        success: function (response) {
+            alert(response);
+            var obj = JSON.parse(response);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+        }
+    })
 }
+
+
+
