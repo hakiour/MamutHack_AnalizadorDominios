4ad1f9a34736533d0d98a42446a5d086dbe5019c
Hamza Akiour <akiourh@gmail.com>
2019-10-06 11:32:00 +0200

    Implementada la conexion con la base de datos, ya funciona el circulo!!

--- web/py/iniciarBusqueda.py
+++ web/py/iniciarBusqueda.py
@@ -28,7 +28,8 @@ def CalculaScore():
 
         tema = datosForm["tema"].value
 
-    url = "https://www.lavanguardia.com/"
+    url = "https://www.tripadvisor.es/ShowTopic-g187427-i42-k3947180-Racismo_en_Espana_La_pregunta_mas_incoherente_que_eh_hecho-Spain.html"
+    #url = "https://www.lavanguardia.com"
     tema="racismo"
 
     DicOdio = csv2Dict(tema+".csv") #'/Dicts/'+'
@@ -54,8 +55,39 @@ mycursor = mydb.cursor()
 sql="INSERT INTO dominio (nombreDominio, tematica) VALUES (%s,%s);"
 val = (resultado["DOMINI"],resultado["TEMATICA"])
 
+
 mycursor.execute(sql,val)
 mydb.commit()
+
+mycursor.close()
+#cojo la id creada
+mycursor = mydb.cursor()
+mycursor.execute("SELECT iddominio FROM dominio ORDER BY fecha DESC LIMIT 1")
+iddominio = mycursor.fetchone()[0]
+mycursor.close()
+
+print(iddominio)
+
+
+for link in resultado["LINKS"]:
+    print(resultado["LINKS"][link])
+    sql="INSERT INTO link (idDominio, indice, nombreLink) VALUES (%s,%s,%s);"
+    mycursor = mydb.cursor()
+    val = (int(iddominio), float(resultado["LINKS"][link]["PUNTUACION"]), link)
+    mycursor.execute(sql,val)
+    mydb.commit()
+    mycursor.close()
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT idlink FROM link ORDER BY idLink DESC LIMIT 1")
+    idlink = mycursor.fetchone()[0]
+    mycursor.close()
+    for palabra in resultado["LINKS"][link][link].keys():
+        sql="INSERT INTO palabras (idLink, Palabra, tipo, numeroVeces) VALUES (%s,%s,%s,%s);"
+        mycursor = mydb.cursor()
+        val = (int(idlink), palabra, "normal", resultado["LINKS"][link][link][palabra])
+        mycursor.execute(sql,val)
+        mydb.commit()
+        mycursor.close()
 
 
 #sql = "INSERT INTO LINK (nombreLink,indice) VALUES (%s,%d)"
